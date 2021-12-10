86a30c5b0828dc47ded09298ee0bb7daac48c844
fred <face@bsc.at>
2021-12-10 18:41:10 +0100

    typos

--- zsh-bsc-aliases.plugin.zsh
+++ zsh-bsc-aliases.plugin.zsh
@@ -56,11 +56,11 @@ alias duf='du -sh *'
 alias gla='glances'
 
 #glab - Gitlab cli
-glabil="glab issue list"
-glabila="glab issue list --all"
-glabiv="glab issue view"
-glabivw="glab issue view --web"
-glabrv="glab repo view --web"
+glabil='glab issue list'
+glabila='glab issue list --all'
+glabiv='glab issue view'
+glabivw='glab issue view --web'
+glabrv='glab repo view --web'
 
 # remote terminal
 alias ssh='TERM=xterm ssh'
