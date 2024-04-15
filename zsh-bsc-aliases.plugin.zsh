5efe44772688cd8b54b04ab2a742b3c42b3e93d0
fred on tuxor <mb@bsc.at>
2024-04-15 15:04:13 +0200

    changed vi to function

--- zsh-bsc-aliases.plugin.zsh
+++ zsh-bsc-aliases.plugin.zsh
@@ -20,7 +20,10 @@ alias ali='alias | fzf'
 alias vm='/usr/bin/nvim'
 alias va='NVIM_APPNAME=nvim-alternate nvim'
 alias vk="NVIM_APPNAME=nvim-kickstart nvim"
-alias vi="NVIM_APPNAME=nvim-lazyvim nvim"
+
+function vi() {
+  NVIM_APPNAME=nvim-lazyvim nvim
+}
 
 function vv() {
   items=( "nvim-alternate" "default" "nvim-kickstart" "nvim-lazyvim" )
