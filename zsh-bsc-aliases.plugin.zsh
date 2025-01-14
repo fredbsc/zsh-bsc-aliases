e4a8c8c8498a8e18e3ccc0f45cbe467bcc0376c4
fred on tuxor <mb@bsc.at>
2025-01-14 06:36:02 +0100

    removed jprev in favour to prev with fzf

--- zsh-bsc-aliases.plugin.zsh
+++ zsh-bsc-aliases.plugin.zsh
@@ -321,12 +321,7 @@ alias p='pet exec'
 alias px='pet search | xc'
 alias pn='pet new'
 alias prev='prev'
-alias jprev='jprev'
 function prev() {
-  PREV=$(fc -lrn | head -n 1)
-  sh -c "pet new `printf %q "$PREV"`"
-}
-function jprev() {
   PREV=$(fc -lrn | fzf)
   sh -c "pet new `printf %q "$PREV"`"
 }
