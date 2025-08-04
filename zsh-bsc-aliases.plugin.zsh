1b8d763956b74437e6e3b51f829d4d2d335fd649
fred on tuxor <mb@bsc.at>
2025-08-04 04:15:21 +0200

    changed pug

--- zsh-bsc-aliases.plugin.zsh
+++ zsh-bsc-aliases.plugin.zsh
@@ -70,7 +70,7 @@ alias mm='pop -t "$(gum filter < ~/scripts/popcontacts.txt)"'
 alias mme='nvim ~/scripts/popcontacts.txt'
 
 # puh - where to got for a quick test
-alias puh='mcdir /tmp/xx_$(date +"%Y%m%d-%H:%M:%S")'
+alias puh='mcdir /tmp/xx_$(date +"%Y%m%d_%H%M%S")'
 alias jpuh='ji $(\ls -d /tmp/xx_* | sort -r | fzf)'
 alias pug='puh && goinit'
 alias pub='puh && bainit'
